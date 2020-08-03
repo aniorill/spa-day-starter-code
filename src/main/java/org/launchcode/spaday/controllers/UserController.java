ed7bc0c3664f977e71582dda5014ff804aab8e54
aniorill <x.iorillo@gmail.com>
2020-08-03 19:24:17 -0400

    Created displayAddUserForm method, added form elements to add.html.

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -2,9 +2,16 @@ package org.launchcode.spaday.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+    
+    @PostMapping("/user/add")
+    public String displayAddUserForm(){
+        return "add";
+    }
+
 }
