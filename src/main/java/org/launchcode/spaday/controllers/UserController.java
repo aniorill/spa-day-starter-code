19b1ded18aaac1f26027833240b854ce28df0bb2
aniorill <x.iorillo@gmail.com>
2020-08-04 20:57:43 -0400

    Added error message, personalized welcome message with link to selection page.

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,17 +1,34 @@
 package org.launchcode.spaday.controllers;
 
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
     
-    @PostMapping("/user/add")
-    public String displayAddUserForm(){
-        return "add";
+    @GetMapping("add")
+    public String displayAddUserForm(Model model){
+
+
+        return "/user/add";
     }
 
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify, @RequestParam String username, @RequestParam String email){
+        model.addAttribute("username", username);
+        model.addAttribute("email", email);
+
+        Boolean passwordCorrect = verify.equals(user.getPassword());
+        if(passwordCorrect){
+            return "/user/index";
+        } else {
+            model.addAttribute("error", "Passwords do not match.");
+            return "/user/add";
+        }
+    }
 }
+
